9a9ea500bd9f629da8aa1e691324c894ffa176a7
agranger <agranger@student.42.fr>
2023-11-30 19:20:14 +0100

    dev script set up

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
+  app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,15 +1,25 @@
 import './App.css';
 import Navbar from './modules/Navbar';
 import Background from './modules/Background';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { addWindow, AppState } from './reducers';
 import { useDispatch } from 'react-redux';
 
 export function App() {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.text())
+      .then(setGreeting);
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
+      <h1>{greeting}</h1>
       <Background />
     </div>
   );
