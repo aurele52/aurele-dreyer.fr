f8971989595488d277d16acd9e16620deba510a9
Mathilde <mathilde.chassignet@gmail.com>
2023-12-24 16:56:10 +0100

    auth: 'get user_info' is ok

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, response } from 'express';
 import { randomBytes } from 'crypto';
+import { ftruncate } from 'fs';
 
 function generateRandomState(): string {
   return randomBytes(16).toString('hex');
@@ -13,60 +14,77 @@ export class AuthController {
   redirectTo42Auth() {
     const api42_id = process.env.API42_ID;
     const api42_callback = process.env.API42_CALLBACK;
-	const state = generateRandomState();
+    const state = generateRandomState();
     const url_auth42 = `https://api.intra.42.fr/oauth/authorize?client_id=${api42_id}&redirect_uri=${api42_callback}&response_type=code&state=${state}`;
     return { url: url_auth42 };
   }
 
-  // To-Do: echanger le code contre l'access token
-  // To-Do: echanger l'access token contre les infos user
-
-
   @Get('/callback') // lors de requete get /callback
-  async callbackFrom42Auth( // declenche fonction
+  async callbackFrom42Auth(
+    // declenche fonction
     @Query('code') code: string, // extrait code
-    @Query('state') state: string,) // extrait state
-//	@Req() request: Request,) 
-	{
-	//	const fullUrl = request.protocol + '://' + request.get('host') + request.originalUrl;
+    @Query('state') state: string,
+  ) {
+    // extrait state
+    //	@Req() request: Request,)
+    //	const fullUrl = request.protocol + '://' + request.get('host') + request.originalUrl;
     //	console.log('URL complète :', fullUrl);
 
-    	const api42_endpoint = 'https://api.intra.42.fr/oauth/token'; //verifier l'adresse
-		// = url utilisee pour echanger le code d'autorisation contre le token 
-		// = là que la requete POST est envoyée (contenant le body avec les infos à echanger)
-    	const body = {
-    	  grant_type: 'authorization_code',
-    	  client_id: process.env.API42_ID,
-    	  client_secret: process.env.API42_SECRET,
-    	  code: code,
-    	  redirect_uri: process.env.API42_CALLBACK,
-    	  state: state,
-    };
+    const api42_endpoint = 'https://api.intra.42.fr/oauth/token'; //verifier l'adresse
+    // = url utilisee pour echanger le code d'autorisation contre le token
 
-    console.log('body: ', body);
-	//envoie de la requete POST avec le body en JSON
-      const response = await fetch(api42_endpoint, { // renvoie objet de type Response qui represente la reponse http à la requete POST
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
+    //envoie de la requete POST avec le body en JSON
+    const access_token = await fetch(api42_endpoint, {
+      // renvoie objet de type Response qui represente la reponse http à la requete POST
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        grant_type: 'authorization_code',
+        client_id: process.env.API42_ID,
+        client_secret: process.env.API42_SECRET,
+        code: code,
+        redirect_uri: process.env.API42_CALLBACK,
+        state: state,
+      }), //le type utilisé pour le corps doit correspondre à l'en-tête "Content-Type"
+    })
+      .then((response) => {
+        if (!response.ok) return Promise.reject(response);
+        return response.json();
+      }) //transforme la réponse JSON reçue en objet JavaScript natif
+      .then((json) => json.access_token) //on extract la variable 'access_token' de ce json
+      .catch((response) => {
+        console.log(
+          'ERROR: could not get access token\nError:',
+          response.status,
+          response.statusText,
+        );
+        response.json().then((json: any) => {
+          console.log(json);
+        });
       });
 
-      console.log('code: ', code);
-      console.log('state: ', state);
-      console.log('response: ', response); //a affiche l'objet reponse
-//      console.log('response status: ', response.status); //donne le code de statut http
-  //    console.log('response body json: ', await response.json()); // corps de la reponse parse sous forme d'objet java script
-
-
-      if (!response.ok) {
-		console.log('response: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAh ', await response.text()); // imprime le texte du corps de la reponse
-		return ("truc");
- //       throw new Error('Network response was not ok');
-      }
-
-      return ({response : await response.json()});
-    // } catch (error) {
-    //   console.log('error: ', error);
-//    }
+    const user_info = await fetch('https://api.intra.42.fr/v2/me', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+      .then((response) => {
+        if (!response.ok) return Promise.reject(response);
+        return response.json();
+      })
+      .catch((response) => {
+        console.log(
+          'ERROR: could not get user info\nError:',
+          response.status,
+          response.statusText,
+        );
+        response.json().then((json: any) => {
+          console.log(json);
+        })
+        return (response.status, response.statusText);
+      });
+      
+    const login = user_info.login;
+    const avatar = user_info.image.versions;
+    return { login, avatar };
   }
 }
