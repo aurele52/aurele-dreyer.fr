3b0209df6230ef3f9af5ff1690e471b7b322d3e1
adesgran <adesgran.dev@gmail.com>
2023-12-24 19:08:56 +0100

    Un beau bordel

--- apps/api/src/friendslist/friendslist.service.ts
+++ apps/api/src/friendslist/friendslist.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -29,9 +29,56 @@ export class FriendslistService {
         userid: user.id,
         username: user.username,
         avatar_url: user.avatar_url,
+        frienship: {
+          status: friend.status,
+          sender_id: friend.user1_id,
+        },
         online: true, ///TO CHANGE
       };
     });
     return friendsList;
+  }
+
+  async sendFriendRequest(senderId: number, receiverId: number) {
+    if (!senderId || !receiverId) {
+      throw new NotFoundException('Invalid senderId or receiverId');
+    }
+
+    const existingFriendship = await this.prisma.friendship.findFirst({
+      where: {
+        OR: [
+          { user1_id: senderId, user2_id: receiverId },
+          { user1_id: receiverId, user2_id: senderId },
+        ],
+      },
+    });
+
+    if (existingFriendship) {
+      if (existingFriendship.status === 'BLOCKED') {
+        throw new NotFoundException('Friendship is blocked');
+      } else if (existingFriendship.status === 'PENDING') {
+        if (existingFriendship.user2_id === senderId) {
+          const updatedFriendship = await this.prisma.friendship.update({
+            where: { id: existingFriendship.id },
+            data: { status: 'FRIENDS' },
+          });
+          return updatedFriendship;
+        } else {
+          throw new NotFoundException('Pending request already exists');
+        }
+      } else {
+        throw new NotFoundException('Friendship already exists');
+      }
+    }
+
+    const newFriendship = await this.prisma.friendship.create({
+      data: {
+        user1_id: senderId,
+        user2_id: receiverId,
+        status: 'PENDING',
+      },
+    });
+
+    return newFriendship;
   }
 }
--- apps/api/src/profile/profile.service.ts
+++ apps/api/src/profile/profile.service.ts
@@ -4,108 +4,116 @@ import { LadderService } from 'src/ladder/ladder.service';
 
 @Injectable()
 export class ProfileService {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly ladder: LadderService
-        ) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly ladder: LadderService,
+  ) {}
 
-    async profile(id: number) {
-        const user = await this.prisma.user.findUnique({
-            where: {
-                id: id,
-            },
-        });
+  async profile(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-        if (!user) {
-            throw new NotFoundException(`User with ID ${id} not found`);
-        }
-
-        const winCount = await this.prisma.matchPlayer.count({
-            where: {
-                user_id: id,
-                winner: true,
-            },
-        });
-
-        const looseCount = await this.prisma.matchPlayer.count({
-            where: {
-                user_id: id,
-                winner: false,
-            },
-        });
-
-        const achievementLvl = await this.prisma.userAchievement.count({
-            where: {
-                user_id: id,
-            }
-        })
-
-        const rank = await this.ladder.getRank(user.id);
-        console.log(rank);
-
-        const res = {
-            id: user.id,
-            username: user.username,
-            avatar_url: user.avatar_url,
-            win_count: winCount,
-            loose_count: looseCount,
-            achievement_lvl: achievementLvl,
-            rank: rank
-        
-        }
-
-        return res;
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
     }
 
-    async historic(id: number) {
-    
-        const matches = await this.prisma.matchPlayer.findMany({
-            where: {
-                user_id: id,
-            },
-            include: {
-                match: {
-                    include: {
-                        players: {
-                            include: {
-                                user: true,
-                            },
-                        },
-                    },
-                },
-            },
-            orderBy: {
-                match: {
-                    updated_at: 'desc',
-                },
-            },
-        });
-    
-        const matchHistory = matches.map((matchPlayer) => {
-            const match = matchPlayer.match;
+    const winCount = await this.prisma.matchPlayer.count({
+      where: {
+        user_id: id,
+        winner: true,
+      },
+    });
 
-            const user1 = match.players[0].user.id === id ? match.players[0].user : match.players[1].user ;
-            const user2 = match.players[0].user.id === id ? match.players[1].user : match.players[0].user ;
-          
-            const player1 = user1.username;
-            const player2 = user2.username;
-          
-            const player1_avatar = user1.avatar_url;
-            const player2_avatar = user2.avatar_url;
-            const score1 = match.players[0].score;
-            const score2 = match.players[1].score;
-    
-            return {
-                id: match.id,
-                player1: player1,
-                player2: player2,
-                player1_avatar: player1_avatar,
-                player2_avatar: player2_avatar,
-                score1: score1,
-                score2: score2,
-            };
-        });
-        return matchHistory;
-    }
-    
+    const looseCount = await this.prisma.matchPlayer.count({
+      where: {
+        user_id: id,
+        winner: false,
+      },
+    });
+
+    const achievementLvl = await this.prisma.userAchievement.count({
+      where: {
+        user_id: id,
+      },
+    });
+
+    const rank = await this.ladder.getRank(user.id);
+    console.log(rank);
+
+    const res = {
+      id: user.id,
+      username: user.username,
+      avatar_url: user.avatar_url,
+      win_count: winCount,
+      loose_count: looseCount,
+      achievement_lvl: achievementLvl,
+      rank: rank,
+    };
+
+    return res;
+  }
+
+  async historic(id: number) {
+    const matches = await this.prisma.matchPlayer.findMany({
+      where: {
+        user_id: id,
+      },
+      include: {
+        match: {
+          include: {
+            players: {
+              include: {
+                user: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        match: {
+          updated_at: 'desc',
+        },
+      },
+    });
+
+    const matchHistory = matches.map((matchPlayer) => {
+      const match = matchPlayer.match;
+
+      const user1 =
+        match.players[0].user.id === id
+          ? match.players[0].user
+          : match.players[1].user;
+      const user2 =
+        match.players[0].user.id === id
+          ? match.players[1].user
+          : match.players[0].user;
+
+      const player1 = user1.username;
+      const player2 = user2.username;
+
+      const player1_id = user1.id;
+      const player2_id = user2.id;
+
+      const player1_avatar = user1.avatar_url;
+      const player2_avatar = user2.avatar_url;
+      const score1 = match.players[0].score;
+      const score2 = match.players[1].score;
+
+      return {
+        id: match.id,
+        player1: player1,
+        player2: player2,
+        player1_id: player1_id,
+        player2_id: player2_id,
+        player1_avatar: player1_avatar,
+        player2_avatar: player2_avatar,
+        score1: score1,
+        score2: score2,
+      };
+    });
+    return matchHistory;
+  }
 }
