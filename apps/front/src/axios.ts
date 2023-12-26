1a1d41b61a25b1c29c1df65dc1cb8538b20aa296
Mathilde <mathilde.chassignet@gmail.com>
2023-12-26 20:10:00 +0100

    auth: feat: axios instance now checks token existence and validity

--- apps/front/src/axios.ts
+++ apps/front/src/axios.ts
@@ -3,24 +3,25 @@ import { router } from "./router";
 
 const api = axios.create({
   baseURL: "/api",
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
 });
 
-// api.interceptors.request.use((req) => {
-//   if (localStorage.getItem("token")) {
-//     req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
-//   }
-//   return req;
-// });
-
-//TO-DO : a tester quand le @Guard sera set-up
-api.interceptors.response.use((res) => {
-  if (res.status === 401) {
-    router.navigate({ to: "/auth" });
+api.interceptors.request.use((req) => {
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   }
-  return res;
+  return req;
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      router.navigate({ to: "/auth" });
+    }
+    return error;
+  }
+);
 
 export default api;
