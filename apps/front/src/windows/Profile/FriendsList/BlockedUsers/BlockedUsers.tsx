ce7285af2536d4363f2d07d5ec58dde2829e8482
adesgran <adesgran.dev@gmail.com>
2023-12-27 13:07:15 +0100

    Fix Empty list

--- apps/front/src/windows/Profile/FriendsList/BlockedUsers/BlockedUsers.tsx
+++ apps/front/src/windows/Profile/FriendsList/BlockedUsers/BlockedUsers.tsx
@@ -9,69 +9,68 @@ import { ReducedUser } from "../../../../shared/ui-components/User/User";
 interface BlockedUsersProps {}
 
 export function BlockedUsers({}: BlockedUsersProps) {
-  const {
-    data: userId,
-    isLoading: userIdLoading,
-    error: userIdError,
-  } = useQuery<number>({
-    queryKey: ["userId"],
-    queryFn: async () => {
-      try {
-        const response = await api.get("/id");
-        return response.data;
-      } catch (error) {
-        console.error("Error fetching userId:", error);
-        throw error;
-      }
-    },
-  });
+	const {
+		data: userId,
+		isLoading: userIdLoading,
+		error: userIdError,
+	} = useQuery<number>({
+		queryKey: ["userId"],
+		queryFn: async () => {
+			try {
+				const response = await api.get("/id");
+				return response.data;
+			} catch (error) {
+				console.error("Error fetching userId:", error);
+				throw error;
+			}
+		},
+	});
 
-  const {
-    data: blockedUsers,
-    isLoading: blockedUsersLoading,
-    error: blockedUsersError,
-  } = useQuery<
-    {
-      id: number;
-    }[]
-  >({
-    queryKey: ["blockedUsers", userId],
-    queryFn: async () => {
-      try {
-        const response = await api.get(`/friendships/blockedList`);
-        return response.data;
-      } catch (error) {
-        console.error("Error fetching BlockedUsers:", error);
-        throw error;
-      }
-    },
-    enabled: !!userId,
-  });
+	const {
+		data: blockedUsers,
+		isLoading: blockedUsersLoading,
+		error: blockedUsersError,
+	} = useQuery<
+		{
+			id: number;
+		}[]
+	>({
+		queryKey: ["blockedUsers", userId],
+		queryFn: async () => {
+			try {
+				const response = await api.get(`/friendships/blockedList`);
+				return response.data;
+			} catch (error) {
+				console.error("Error fetching BlockedUsers:", error);
+				throw error;
+			}
+		},
+		enabled: !!userId,
+	});
 
-  if (blockedUsersLoading || userIdLoading) {
-    return <FaSpinner className="loadingSpinner" />;
-  }
+	if (blockedUsersLoading || userIdLoading) {
+		return <FaSpinner className="loadingSpinner" />;
+	}
 
-  if (blockedUsersError) {
-    return <div>Error loading users: {blockedUsersError.message}</div>;
-  }
+	if (blockedUsersError) {
+		return <div>Error loading users: {blockedUsersError.message}</div>;
+	}
 
-  if (userIdError) {
-    return <div>Error loading user: {userIdError.message}</div>;
-  }
+	if (userIdError) {
+		return <div>Error loading user: {userIdError.message}</div>;
+	}
 
-  return (
-    <div className="BlockedUsersComponent">
-      <div className="Body">
-        <List>
-          <div className="SectionName">Received</div>
-          {blockedUsers?.map((user, key) => (
-            <ReducedUser key={key} userId={user.id}>
-              <Button icon="Unblock" color="pink" />
-            </ReducedUser>
-          ))}
-        </List>
-      </div>
-    </div>
-  );
+	return (
+		<div className="BlockedUsersComponent">
+			<div className="Body">
+				<List>
+					{blockedUsers?.map((user, key) => (
+						<ReducedUser key={key} userId={user.id}>
+							<Button icon="Unblock" color="pink" />
+						</ReducedUser>
+					))}
+				</List>
+			</div>
+		</div>
+	);
 }
