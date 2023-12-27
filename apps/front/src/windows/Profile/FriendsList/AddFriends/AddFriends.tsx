b624c9dc994038e104b4bfc060d50326915195b2
adesgran <adesgran.dev@gmail.com>
2023-12-27 14:39:57 +0100

    Button Heart include

--- apps/front/src/windows/Profile/FriendsList/AddFriends/AddFriends.tsx
+++ apps/front/src/windows/Profile/FriendsList/AddFriends/AddFriends.tsx
@@ -1,73 +1,87 @@
 import "./AddFriends.css";
-import api from "../../../../axios"
+import api from "../../../../axios";
 import List from "../../../../shared/ui-components/List/List";
-import { Button } from "../../../../shared/ui-components/Button/Button";
+import {
+	Button,
+	HeartButton,
+} from "../../../../shared/ui-components/Button/Button";
 import { ReducedUser } from "../../../../shared/ui-components/User/User";
 import { useEffect, useState } from "react";
 
 export function AddFriends() {
-  const [placeholderValue, setPlaceholderValue] = useState<string>("");
-  const [users, setUsers] = useState<number[] | null>(null);
+	const [placeholderValue, setPlaceholderValue] = useState<string>("");
+	const [users, setUsers] = useState<
+		{ id: number; username: string }[] | null
+	>(null);
 
-  useEffect(() => {
-    const storedPlaceholderValue = localStorage.getItem("placeholderValue");
-    const storedUsers = localStorage.getItem("users");
+	useEffect(() => {
+		const storedPlaceholderValue = localStorage.getItem("placeholderValue");
+		const storedUsers = localStorage.getItem("users");
 
-    if (storedPlaceholderValue) {
-      setPlaceholderValue(storedPlaceholderValue);
-    }
+		if (storedPlaceholderValue) {
+			setPlaceholderValue(storedPlaceholderValue);
+		}
 
-    if (storedUsers) {
-      setUsers(JSON.parse(storedUsers));
-    }
-  }, []);
+		if (storedUsers) {
+			setUsers(JSON.parse(storedUsers));
+		}
+	}, []);
 
-  useEffect(() => {
-    localStorage.setItem("placeholderValue", placeholderValue);
-    localStorage.setItem("users", JSON.stringify(users));
-  }, [placeholderValue, users]);
+	useEffect(() => {
+		localStorage.setItem("placeholderValue", placeholderValue);
+		localStorage.setItem("users", JSON.stringify(users));
+	}, [placeholderValue, users]);
 
-  const handleButtonClick = async () => {
-    try {
-      const response = await api.get<number[]>(
-        "/friendslist/potentialFriends",
-        {
-          params: {
-            placeholderValue: placeholderValue,
-          },
-        }
-      );
-      setUsers(response.data);
-    } catch (error) {
-      console.error("Error making API request:", error);
-    }
-  };
+	const handleButtonClick = async () => {
+		try {
+			const response = await api.get<{ id: number; username: string }[]>(
+				"/friendslist/potentialFriends",
+				{
+					params: {
+						placeholderValue: placeholderValue,
+					},
+				}
+			);
+			setUsers(response.data);
+		} catch (error) {
+			console.error("Error making API request:", error);
+		}
+	};
 
-  return (
-    <div className="AddFriendsModule">
-      <div className="Header">
-        <div className="SearchBar">
-          <div className="TypeBar">
-            <input
-              className="Placeholder"
-              value={placeholderValue}
-              onChange={(e) => setPlaceholderValue(e.target.value)}
-            />
-            <Button icon="Lens" color="purple" onClick={handleButtonClick} />
-          </div>
-        </div>
-      </div>
-      <div className="Body">
-        <List dark={false}>
-          {users?.map((id, key) => {
-            return (
-              <ReducedUser userId={id} key={key}>
-                <Button icon="Heart" color="pink" />
-              </ReducedUser>
-            );
-          })}
-        </List>
-      </div>
-    </div>
-  );
+	return (
+		<div className="AddFriendsModule">
+			<div className="Header">
+				<div className="SearchBar">
+					<div className="TypeBar">
+						<input
+							className="Placeholder"
+							value={placeholderValue}
+							onChange={(e) =>
+								setPlaceholderValue(e.target.value)
+							}
+						/>
+						<Button
+							icon="Lens"
+							color="purple"
+							onClick={handleButtonClick}
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="Body">
+				<List dark={false}>
+					{users?.map((user, key) => {
+						return (
+							<ReducedUser userId={user.id} key={key}>
+								<HeartButton
+									userId={user.id}
+									username={user.username}
+								/>
+							</ReducedUser>
+						);
+					})}
+				</List>
+			</div>
+		</div>
+	);
 }
