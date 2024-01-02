1660b81bfedee5afeb8d16e43928ffaa74927707
adesgran <adesgran.dev@gmail.com>
2024-01-02 13:06:57 +0100

    Can't interact with the page when there is a modal

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,14 +1,37 @@
 import "./App.css";
 import Navbar from "./main-page/Navbar/Navbar";
 import Background from "./main-page/Background/Background";
+import { AppState } from "./reducers";
+import { useSelector } from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Background />
-    </div>
-  );
+	const { displayFilter, zIndexFilter } = useSelector((state: AppState) => {
+		const modalWindow = state.windows.find(
+			(window) => window.content.type === "MODAL"
+		);
+
+		if (modalWindow) {
+			return {
+				displayFilter: "block",
+				zIndexFilter: modalWindow.zindex || 0,
+			};
+		} else {
+			return {
+				displayFilter: "none",
+				zIndexFilter: 0,
+			};
+		}
+	});
+	return (
+		<div className="App">
+			<div
+				className="Filter"
+				style={{ display: displayFilter, zIndex: zIndexFilter }}
+			></div>
+			<Navbar />
+			<Background />
+		</div>
+	);
 }
 
 export default App;
