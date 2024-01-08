988f4e95b9151c6079d0bb1290d087b75301cc09
adesgran <adesgran.dev@gmail.com>
2024-01-08 18:37:34 +0100

    Fix Auth2FA

--- apps/front/src/auth-pages/Auth2FA/Auth2FA.tsx
+++ apps/front/src/auth-pages/Auth2FA/Auth2FA.tsx
@@ -9,72 +9,75 @@ import { useMutation } from "@tanstack/react-query";
 import api from "../../axios.ts";
 
 interface ValidationErrorResponse {
-  [key: string]: string[];
+	[key: string]: string[];
 }
 
 export default function Auth2FA() {
-  const { id } = useParams({ strict: false });
+	const { id } = useParams({ strict: false });
 
-  const { mutate: postForm, error } = useMutation({
-    mutationFn: (code: string) => {
-      return api
-        .post(`/auth/2fa/submit?jwt_id=${id}`, {
-          code,
-        })
-        .then((response) => response.data.token)
-        .catch((error: AxiosError) => {
-          if (error.response?.status === 400)
-            throw error.response?.data as ValidationErrorResponse;
-          else
-            throw {
-              code: [(error.response?.data as { message: string }).message],
-            };
-        });
-    },
-    onSuccess: (token: string) => {
-      router.navigate({ to: "/auth/redirect/$token", params: { token } });
-    },
-  });
+	const { mutate: postForm, error } = useMutation({
+		mutationFn: (code: string) => {
+			return api
+				.post(`/auth/2fa/submit?jwt_id=${id}`, {
+					code,
+				})
+				.then((response) => response.data.token)
+				.catch((error: AxiosError) => {
+					if (error.response?.status === 400)
+						throw error.response?.data as ValidationErrorResponse;
+					else
+						throw {
+							code: [
+								(error.response?.data as { message: string })
+									.message,
+							],
+						};
+				});
+		},
+		onSuccess: (token: string) => {
+			router.navigate({ to: "/redirect/$token", params: { token } });
+		},
+	});
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const target = e.target as HTMLFormElement;
-    const formData = Object.fromEntries(new FormData(target));
-    postForm(formData.code as string);
-  };
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const target = e.target as HTMLFormElement;
+		const formData = Object.fromEntries(new FormData(target));
+		postForm(formData.code as string);
+	};
 
-  const handleBackToSignIn = () => {
-    router.navigate({ to: "/auth" });
-  };
+	const handleBackToSignIn = () => {
+		router.navigate({ to: "/auth" });
+	};
 
-  return (
-    <>
-      <div className="twofa-title">
-        Two factor authentication
-        <div>Check your Google Authenticator app!</div>
-      </div>
-      {/* <div id="code2fa-error"></div> */}
-      <form id="twofa-form" onSubmit={handleSubmit}>
-        <Input
-          label="Enter your code:"
-          errors={(error as any)?.code}
-          type="number"
-          placeholder="6 to 8 digits code"
-          id="validation-code"
-          name="code"
-          required
-          minLength={6}
-          maxLength={8}
-        ></Input>
-        <Button content="submit" color="pink" type="submit" />
-      </form>
-      <div className="rightside-button">
-        <Button
-          content="back to sign in"
-          color="red"
-          onClick={() => handleBackToSignIn()}
-        />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="twofa-title">
+				Two factor authentication
+				<div>Check your Google Authenticator app!</div>
+			</div>
+			{/* <div id="code2fa-error"></div> */}
+			<form id="twofa-form" onSubmit={handleSubmit}>
+				<Input
+					label="Enter your code:"
+					errors={(error as any)?.code}
+					type="number"
+					placeholder="6 to 8 digits code"
+					id="validation-code"
+					name="code"
+					required
+					minLength={6}
+					maxLength={8}
+				></Input>
+				<Button content="submit" color="pink" type="submit" />
+			</form>
+			<div className="rightside-button">
+				<Button
+					content="back to sign in"
+					color="red"
+					onClick={() => handleBackToSignIn()}
+				/>
+			</div>
+		</>
+	);
 }
