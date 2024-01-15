0c4aa24a4159f250b5f4c4ca6edbb95016b98d81
audreyer <aureledreyer67@outlook.fr>
2024-01-16 00:26:50 +0100

    audreyer: responsive working and few fix in game menu

--- apps/api/src/game/pong.gateway.ts
+++ apps/api/src/game/pong.gateway.ts
@@ -35,16 +35,6 @@ export class PongGateway {
     }
   }
 
-  @SubscribeMessage('client.closeJoinCustom')
-  handleCloseJoinCustom(client: Socket) {
-    client.emit('server.closeJoinCustom');
-  }
-
-  @SubscribeMessage('client.closeCreateCustom')
-  handleCloseCreateCustom(client: Socket) {
-    client.emit('server.closeCreateCustom');
-  }
-
   @SubscribeMessage('client.closeMainWindow')
   handleCloseMainWindow(client: Socket) {
     const index = this.connectedClient.findIndex((value) => {
@@ -136,7 +126,6 @@ export class PongGateway {
         bally: gameData.gameysize / 2,
       };
       this.lobbyManager.createCustomLobby(this.connectedClient[index]);
-      client.emit('server.matchLoading');
     }
   }
 
