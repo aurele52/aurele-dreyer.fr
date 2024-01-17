1cd214aafebbd211adde0f3d8649f87a265353b1
agranger <agranger@student.42.fr>
2024-01-17 17:32:04 +0100

    bug fixed

--- apps/api/src/game/lobby/privateLobbyManager.ts
+++ apps/api/src/game/lobby/privateLobbyManager.ts
@@ -8,7 +8,7 @@ export class privateLobbyManager {
 
   public addToPrivateQueue(client: clientInfo, id: number) {
     console.log('SIZE: ', this.privateLobbies.length);
-    console.log('user1: ', client.user'taraget: ', id, this.privateLobbies.length);
+    console.log('user1: ', client.user, 'taraget: ', id, this.privateLobbies.length);
     client.status = 'waiting join private';
     client.mode = 'private';
     const index = this.privateLobbies.findIndex((value) => {
--- apps/front/src/socket.tsx
+++ apps/front/src/socket.tsx
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const domainNameBack = "http://localhost:3000";
+console.log("Process Env", process.env);
+
+const domainNameBack = process.env.REACT_APP_DOMAIN_NAME || "http://localhost:3000";
 
 // "undefined" means the URL will be computed from the `window.location` object
 export const socket = io(domainNameBack, {
